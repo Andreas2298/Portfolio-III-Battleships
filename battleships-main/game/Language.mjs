d7df365900e811d4669b86f1701aca08a38f3f70
Andreas2298 <69863018+Andreas2298@users.noreply.github.com>
2024-11-10 21:16:27 +0100

    Added English and Norwegian game commands.
    Inside the DICTIONARY, I added local identfiers for English and Norwegian called "no" and "en". Then added keys and translations for different commands in the game.

--- battleships-main/game/Language.mjs
+++ battleships-main/game/Language.mjs
@@ -1,13 +1,41 @@
 const DICTIONARY = {
 
+const DICTIONARY = {
+  en: {
+    startgame: "Start game",
 
+    exitGame: "Exit the game",
 
+    selectYourPreferredLanguage: "Select your preferred language",
 
+    shipPlacementFirstPlayer:
+      "SHIP PLACEMENT\nFirst player get ready.\nPlayer two look away",
 
+    shipPlacementSecondPlayer:
+      "SHIP PLACEMENT\nSecond player get ready.\nPlayer one look away",
 
+    languageName: "English",
 
+    preferredLanguage: "English",
+  },
 
+no: {
+    startGame: "Start spillet",
 
+    exitGame: "Avslutt spillet",
+
+    selectYourPreferredLanguage: "Velg ditt foretrukne språk",
+
+    shipPlacementFirstPlayer:
+      "SKIPSPLASSERING\nFørstespiller gjør deg klar.\nSpiller to ser bort",
+
+    shipPlacementSecondPlayer:
+      "SKIPSPLASSERING\nAndre spiller gjør deg klar.\nSpiller en se bort",
+
+    languageName: "Norsk",
+
+    preferredLanguage: "Norsk",
+  },
 };
 
   export default DICTIONARY;
